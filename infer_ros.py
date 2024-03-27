755296908bca34fe024ea718db172c4fc2c0a05e
07hokage <saihaneesh.allu@gmail.com>
2024-03-27 10:46:18 -0500

    infer, segment, overlay operations

--- infer_ros.py
+++ infer_ros.py
@@ -15,8 +15,6 @@ import numpy as np
 
 import torch
 from PIL import Image
-from PIL import ImageFilter
-
 from torch.autograd import Variable
 from torchvision import transforms
 
@@ -26,89 +24,66 @@ from gdnet import GDNet
 
 from image_listener import ImageListener
 import rospy
+import cv2
 
-device_ids = [0]
-torch.cuda.set_device(device_ids[0])
+class Inference:
+    def __init__(self) -> None:
+        
+        self.device_ids = [0]
+        torch.cuda.set_device(self.device_ids[0])
 
-print(f"infer starts\n")
-ckpt_path = './ckpt'
-exp_name = 'GDNet'
-args = {
-    'snapshot': '200',
-    'scale': 416,
-    # 'crf': True,
-    'crf': False,
-}
+        print(f"infer starts\n")
+        self.ckpt_path = './ckpt'
+        self.exp_name = 'GDNet'
+        self.args = {
+            'snapshot': '200',
+            'scale': 416,
+            # 'crf': True,
+            'crf': False,
+        }
 
-print(torch.__version__)
+        print(torch.__version__)
 
-img_transform = transforms.Compose([
-    transforms.Resize((args['scale'], args['scale'])),
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-])
+        self.img_transform = transforms.Compose([
+            transforms.Resize((self.args['scale'], self.args['scale'])),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
 
-to_test = {'GDD': gdd_testing_root}
+        self.to_test = {'GDD': gdd_testing_root}
 
-to_pil = transforms.ToPILImage()
+        self.to_pil = transforms.ToPILImage()
+        self.net = GDNet()
+        self.kernel = np.ones((3,3),np.uint8)
 
-
-def main():
-    print(f"enter main function\n")
-    print(f"devcie id : {device_ids[0]}")
-    # net = GDNet().cuda(device_ids[0])
-    net = GDNet()
-    rospy.init_node("img_listen_n_infer")
-    listen = ImageListener("Gazebo")
-
-
-    if len(args['snapshot']) > 0:
-        print(f"checking snapshots\n")
-        print('Load snapshot {} for testing'.format(args['snapshot']))
-        net.load_state_dict(torch.load(os.path.join(ckpt_path, exp_name, args['snapshot'] + '.pth')))
-        print('Load {} succeed!'.format(os.path.join(ckpt_path, exp_name, args['snapshot'] + '.pth')))
-
-    net.eval()
-    with torch.no_grad():
-        for name, root in to_test.items():
-            count = 0
-            while True:
-                try:
-                    clr, dpt, cam,intri = listen.get_data()
-                    non_z_depth = np.where(dpt > 0) 
-                    check_mkdir(os.path.join(gdd_results_root, '%s_%s' % (exp_name, args['snapshot'])))
-                    img = Image.fromarray(clr)
-                    if img.mode != 'RGB':
-                        img = img.convert('RGB')
-                        print("{} is a gray image.".format(name))
-                    w, h = img.size
-                    # img_var = Variable(img_transform(img).unsqueeze(0)).cuda(device_ids[0])
-                    img_var = Variable(img_transform(img).unsqueeze(0))
-                    f1, f2, f3 = net(img_var)
-                    f1 = f1.data.squeeze(0).cpu()
-                    f2 = f2.data.squeeze(0).cpu()
+    def main(self):
+        rospy.init_node("img_listen_n_infer")
+        self.listener = ImageListener("Gazebo")
+        if len(self.args['snapshot']) > 0:
+            print(f"checking snapshots\n")
+            print('Load snapshot {} for testing'.format(self.args['snapshot']))
+            self.net.load_state_dict(torch.load(os.path.join(self.ckpt_path, self.exp_name, self.args['snapshot'] + '.pth')))
+            print('Load {} succeed!'.format(os.path.join(self.ckpt_path, self.exp_name, self.args['snapshot'] + '.pth')))
+            self.net.eval()
+            with torch.no_grad():
+                while not rospy.is_shutdown():
+                    rgb, depth, _, _ = self.listener.get_data()
+                    rgb = Image.fromarray(rgb)
+                    w,h = rgb.size
+                    img_var = Variable(self.img_transform(rgb).unsqueeze(0))
+                    f1, f2, f3 = self.net(img_var)
                     f3 = f3.data.squeeze(0).cpu()
-                    f1 = np.array(transforms.Resize((h, w))(to_pil(f1)))
-                    f2 = np.array(transforms.Resize((h, w))(to_pil(f2)))
-                    f3 = np.array(transforms.Resize((h, w))(to_pil(f3)))
-                    if args['crf']:
-                        # f1 = crf_refine(np.array(img.convert('RGB')), f1)
-                        # f2 = crf_refine(np.array(img.convert('RGB')), f2)
-                        f3 = crf_refine(np.array(img.convert('RGB')), f3)
+                    f3 = np.array(transforms.Resize((h, w))(self.to_pil(f3)))
+                    if self.args['crf']:
+                        f3 = crf_refine(np.array(rgb.convert('RGB')), f3)
+                    print(f"type of f3 {type(f3)}")
+                    non_z_depth = np.where(depth > 0)
                     f3[non_z_depth] = 0
-                    # Image.fromarray(f1).save(os.path.join(ckpt_path, exp_name, '%s_%s' % (exp_name, args['snapshot']),
-                    #                                       img_name[:-4] + "_h.png"))
-                    # Image.fromarray(f2).save(os.path.join(ckpt_path, exp_name, '%s_%s' % (exp_name, args['snapshot']),
-                    #                                       img_name[:-4] + "_l.png"))
-                    f3 = Image.fromarray(f3)
-                    
-                    f3 = f3.filter(ImageFilter.MinFilter(9))
-                    f3.save(os.path.join(gdd_results_root, '%s_%s' % (exp_name, args['snapshot']),
-                                                        str(count)+ ".png"))
-                    count += 1
-                except KeyboardInterrupt:
-                    break
+                    f3 = cv2.morphologyEx(f3, cv2.MORPH_CLOSE, self.kernel)
+                    rospy.sleep(1)
 
 
 if __name__ == '__main__':
-    main()
+    infy = Inference()
+    infy.main()
+    rospy.spin()
